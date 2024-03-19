1af128ef1c748a9dd9b5694c008bb7c2a8ad1b60
DillanMilo <dillanxx@icloud.com>
2024-03-19 16:06:12 +0200

    Update page title

--- src/components/Emoji.tsx
+++ src/components/Emoji.tsx
@@ -16,7 +16,7 @@ const Emoji = ({ rating }: Props) => {
     5: { src: bullsEye, alt: "exceptional", boxSize: "35px" },
   };
 
-  <Image {...emojiMap[rating]} marginTop={1} />;
+  return <Image {...emojiMap[rating]} marginTop={1} />;
 };
 
 export default Emoji;
