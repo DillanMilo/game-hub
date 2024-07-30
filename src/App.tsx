f1c2db8c457a976249c452a0752beb9835894428
DillanMilo <dillanxx@icloud.com>
2024-07-30 17:55:34 +0200

    add lazy loading components and  suspense fallback

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,14 @@
-import {
-  Box,
-  Flex,
-  Grid,
-  GridItem,
-  HStack,
-  Show,
-  Text,
-} from "@chakra-ui/react";
-import NavBar from "./components/NavBar";
-import GameGrid from "./components/GameGrid";
-import GenreList from "./components/GenreList";
-import { useState } from "react";
+import { Box, Flex, Grid, GridItem, Show, Text } from "@chakra-ui/react";
+import React, { Suspense, lazy, useState } from "react";
 import { Genre } from "./hooks/UseGenres";
 import { Platform } from "./hooks/useGames";
-import PlatformSelector from "./components/Platfromselector";
-import SortSelector from "./components/SortSelector";
-import GameHeading from "./components/GameHeading";
+
+const NavBar = lazy(() => import("./components/NavBar"));
+const GameGrid = lazy(() => import("./components/GameGrid"));
+const GenreList = lazy(() => import("./components/GenreList"));
+const PlatformSelector = lazy(() => import("./components/Platfromselector"));
+const SortSelector = lazy(() => import("./components/SortSelector"));
+const GameHeading = lazy(() => import("./components/GameHeading"));
 
 export interface GameQuery {
   genre: Genre | null;
@@ -33,64 +26,68 @@ function App() {
   });
 
   return (
-    <Grid
-      templateAreas={{
-        base: `"nav" "main"`,
-        lg: `"nav nav" "aside main"`,
-      }}
-      templateColumns={{
-        base: "1fr",
-        lg: "200px 1fr",
-      }}
-    >
-      <GridItem area="nav">
-        <NavBar
-          onSearch={(searchText) => setGameQuery({ ...gameQuery, searchText })}
-        />
-      </GridItem>
-      <Show above="lg">
-        <GridItem area="aside" paddingX={"5"}>
-          <GenreList
-            selectedGenre={gameQuery.genre}
-            onSelectGenre={(genre) => setGameQuery({ ...gameQuery, genre })}
+    <Suspense fallback={<div>Loading...</div>}>
+      <Grid
+        templateAreas={{
+          base: `"nav" "main"`,
+          lg: `"nav nav" "aside main"`,
+        }}
+        templateColumns={{
+          base: "1fr",
+          lg: "200px 1fr",
+        }}
+      >
+        <GridItem area="nav">
+          <NavBar
+            onSearch={(searchText) =>
+              setGameQuery({ ...gameQuery, searchText })
+            }
           />
         </GridItem>
-      </Show>
-      <GridItem area="main">
-        <Box paddingLeft={2}>
-          <GameHeading gameQuery={gameQuery} />
-          <Flex marginBottom={5}>
-            <Box marginRight={5}>
-              <PlatformSelector
-                selectedPlatform={gameQuery.platform}
-                onSelectPlatform={(platform) =>
-                  setGameQuery({ ...gameQuery, platform })
+        <Show above="lg">
+          <GridItem area="aside" paddingX={"5"}>
+            <GenreList
+              selectedGenre={gameQuery.genre}
+              onSelectGenre={(genre) => setGameQuery({ ...gameQuery, genre })}
+            />
+          </GridItem>
+        </Show>
+        <GridItem area="main">
+          <Box paddingLeft={2}>
+            <GameHeading gameQuery={gameQuery} />
+            <Flex marginBottom={5}>
+              <Box marginRight={5}>
+                <PlatformSelector
+                  selectedPlatform={gameQuery.platform}
+                  onSelectPlatform={(platform) =>
+                    setGameQuery({ ...gameQuery, platform })
+                  }
+                />
+              </Box>
+              <SortSelector
+                sortOrder={gameQuery.sortOrder}
+                onSelectSortOrder={(sortOrder) =>
+                  setGameQuery({ ...gameQuery, sortOrder })
                 }
               />
-            </Box>
-            <SortSelector
-              sortOrder={gameQuery.sortOrder}
-              onSelectSortOrder={(sortOrder) =>
-                setGameQuery({ ...gameQuery, sortOrder })
-              }
-            />
-          </Flex>
-        </Box>
-        {gameQuery.platform?.slug === "playstation" ? (
-          <Box textAlign="center" padding="10px">
-            <Text fontSize="xl" color="red.500">
-              The PlayStation section is currently under maintenance. Please
-              check back later.
-            </Text>
+            </Flex>
           </Box>
-        ) : (
-          <GameGrid
-            gameQuery={gameQuery}
-            selectedPlatform={gameQuery.platform}
-          />
-        )}
-      </GridItem>
-    </Grid>
+          {gameQuery.platform?.slug === "playstation" ? (
+            <Box textAlign="center" padding="10px">
+              <Text fontSize="xl" color="red.500">
+                The PlayStation section is currently under maintenance. Please
+                check back later.
+              </Text>
+            </Box>
+          ) : (
+            <GameGrid
+              gameQuery={gameQuery}
+              selectedPlatform={gameQuery.platform}
+            />
+          )}
+        </GridItem>
+      </Grid>
+    </Suspense>
   );
 }
 
