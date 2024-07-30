806a49d7df18397d3069a46d5861faa1c303e828
DillanMilo <dillanxx@icloud.com>
2024-07-30 14:03:38 +0200

    fix light mode, dark mode toggle button

--- src/components/ColorModeSwitch.tsx
+++ src/components/ColorModeSwitch.tsx
@@ -1,4 +1,10 @@
-import { HStack, Switch, Text, useColorMode } from "@chakra-ui/react";
+import {
+  HStack,
+  Switch,
+  Text,
+  useColorMode,
+  useColorModeValue,
+} from "@chakra-ui/react";
 
 const ColorModeSwitch = () => {
   const { toggleColorMode, colorMode } = useColorMode();
@@ -9,8 +15,19 @@ const ColorModeSwitch = () => {
         colorScheme="purple"
         isChecked={colorMode === "dark"}
         onChange={toggleColorMode}
+        borderColor={useColorModeValue("black", "gray.800")}
+        bg={useColorModeValue("blue.100", "switcher")}
+        borderRadius={"full"}
+        _hover={{
+          borderColor: useColorModeValue("black", "gray.800"),
+        }}
+        _focus={{
+          borderColor: useColorModeValue("black", "gray.800"),
+        }}
       />
-      <Text whiteSpace="nowrap">Dark Mode</Text>
+      <Text whiteSpace="nowrap" color={useColorModeValue("gray.800", "white")}>
+        {colorMode === "dark" ? "Dark Mode" : "Light Mode"}
+      </Text>
     </HStack>
   );
 };
